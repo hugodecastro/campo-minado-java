c8579719b667d1f89914d3cc74305bff43c3d7d3
Hugo Castro <hugodecastro63@gmail.com>
2020-10-14 21:24:03 -0300

    implementado classe Campo e testes unitarios

---
+++ src/com/campominado/excecao/ExplosaoException.java
@@ -0,0 +1,7 @@
+package com.campominado.excecao;
+
+public class ExplosaoException extends RuntimeException{
+
+	private static final long serialVersionUID = 1441931876834153053L;
+	
+}
---
+++ teste/com/campominado/modelo/CampoTeste.java
@@ -0,0 +1,88 @@
+package com.campominado.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Esquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Direita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1EmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1EmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaDiagonalCimaEsquerda() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaDiagonalCimaDireita() {
+		Campo vizinho = new Campo(2, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaDiagonalBaixoEsquerda() {
+		Campo vizinho = new Campo(4, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaDiagonalBaixoDireita() {
+		Campo vizinho = new Campo(4, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertFalse(resultado);
+	}
+	
+}
